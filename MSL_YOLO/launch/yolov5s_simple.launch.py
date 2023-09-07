2ef66587f8657947c8c1a268636baae3a77daebd
SpacePanda-42 <77905853+SpacePanda-42@users.noreply.github.com>
2023-09-07 14:16:30 -0700

    removed unnecessary launches

--- MSL_YOLO/launch/yolov5s_simple.launch.py
+++ MSL_YOLO/launch/yolov5s_simple.launch.py
@@ -7,13 +7,6 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
     yolox_ros_share_dir = get_package_share_directory('MSL_YOLO')
 
-    webcam = launch_ros.actions.Node(
-        package="v4l2_camera", executable="v4l2_camera_node",
-        parameters=[
-            {"image_size": [640,480]},
-        ],
-    )
-
     MSL_YOLO = launch_ros.actions.Node(
         package="MSL_YOLO", executable="MSL_YOLO",
         parameters=[
@@ -22,11 +15,6 @@ def generate_launch_description():
 
     )
 
-    rqt_graph = launch_ros.actions.Node(
-        package="rqt_graph", executable="rqt_graph",
-    )
-
     return launch.LaunchDescription([
-        webcam,
         MSL_YOLO,
     ])
