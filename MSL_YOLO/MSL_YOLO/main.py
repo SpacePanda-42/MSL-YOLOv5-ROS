05ff218180143ab8426b7433c5a4d696c1163225
SpacePanda-42 <77905853+SpacePanda-42@users.noreply.github.com>
2023-09-07 14:18:53 -0700

    fixed subscriber topic

--- MSL_YOLO/MSL_YOLO/main.py
+++ MSL_YOLO/MSL_YOLO/main.py
@@ -189,7 +189,7 @@ class MSL_YOLO(Node):
         self.pub_image = self.create_publisher(Image, 'yolov5/image_raw', 10)
         self.pub_depths = self.create_publisher(Float32MultiArray, 'yolov5/detected_object_depths', 10)
 
-        self.sub_image = self.create_subscription(Image, 'image_raw', self.image_callback, 10)
+        self.sub_image = self.create_subscription(Image, 'color/image', self.image_callback, 10)
         self.depth_image = self.create_subscription(Image, 'stereo/converted_depth', self.depth_callback, 10)
 
         # parameter
